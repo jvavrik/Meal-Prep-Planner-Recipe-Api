e47da7f259ef478df5de216c6856beaf101f8eb8
James Vavrik <jamesvavrikjr@gmail.com>
2025-10-15 21:00:43 -0500

    adding prostres migrations and docker-compose

---
+++ RecipeApi/Data/RecipeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeApi.Models;
+
+namespace RecipeApi.Data
+{
+    public class RecipeDbContext : DbContext
+    {
+        public RecipeDbContext(DbContextOptions<RecipeDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Recipe> Recipes => Set<Recipe>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
